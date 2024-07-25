ae2daf74d54b02e55332b37b4b0ed8759ddd48c8
Cheranga Hatangala <cheranga@gmail.com>
2024-07-25 22:56:59 +1000

    adding first Playwright test

---
+++ GettingStarted/Playwright/LandingPageTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.Playwright.MSTest;
+
+
+[TestClass]
+public class LandingPageTests : PageTest
+{
+    [TestMethod(displayName:"Check title contains Playwright")]
+    public async Task HasTitle()
+    {
+        // Arrange
+        // Browse to the home page of Playwright.dev
+        var url = "https://playwright.dev";
+
+        // Act (Arrange and act are the same)
+        var response = await Page.GotoAsync(url);
+
+        // Assert whether the title contains "Playwright"
+        Assert.AreEqual(200, response.Status);
+        await Expect(Page).ToHaveTitleAsync(new Regex("Playwright"));
+
+    }
+}
---
+++ GettingStarted/Playwright/ProjectBase.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.Playwright;
+using Microsoft.Playwright.MSTest;
+
+namespace GettingStarted.Playwright;
+
+public class ProjectBase : PageTest
+{
+    // TODO: write helper methods for your project
+    public async Task CheckTitle( string expectedTitle)
+    {
+        await Expect(Page).ToHaveTitleAsync(new Regex(expectedTitle));
+    }
+
+    public void IsSuccessfulStatus(IResponse response)
+    {
+        Assert.IsTrue(response.Status == 200);
+    }
+}
