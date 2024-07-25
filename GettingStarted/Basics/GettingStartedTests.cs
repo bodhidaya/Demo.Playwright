10f0e640432235f749af53b92a8283848674463a
Cheranga Hatangala <cheranga@gmail.com>
2024-07-25 22:15:05 +1000

    cleaning up unwanted using statements

--- GettingStarted/Basics/GettingStartedTests.cs
+++ GettingStarted/Basics/GettingStartedTests.cs
@@ -2,32 +2,29 @@ using System.Text.RegularExpressions;
 using Microsoft.Playwright;
 using Microsoft.Playwright.MSTest;
 
+namespace GettingStarted.Basics;
 
-
-namespace GettingStarted.Basics
+[TestClass]
+public class LandingPageTests : PageTest
 {
-    [TestClass]
-    public class LandingPageTests : PageTest
+    [TestMethod]
+    public async Task HasTitle()
     {
-        [TestMethod]
-        public async Task HasTitle()
-        {
-            await Page.GotoAsync("https://playwright.dev");
+        await Page.GotoAsync("https://playwright.dev");
 
-            // Expect a title "to contain" a substring.
-            await Expect(Page).ToHaveTitleAsync(new Regex("Playwright"));
-        }
+        // Expect a title "to contain" a substring.
+        await Expect(Page).ToHaveTitleAsync(new Regex("Playwright"));
+    }
 
-        [TestMethod]
-        public async Task GetStartedLink()
-        {
-            await Page.GotoAsync("https://playwright.dev");
+    [TestMethod]
+    public async Task GetStartedLink()
+    {
+        await Page.GotoAsync("https://playwright.dev");
 
-            // Click the get started link.
-            await Page.GetByRole(AriaRole.Link, new() { Name = "Get started" }).ClickAsync();
+        // Click the get started link.
+        await Page.GetByRole(AriaRole.Link, new PageGetByRoleOptions {Name = "Get started"}).ClickAsync();
 
-            // Expects page to have a heading with the name of Installation.
-            await Expect(Page.GetByRole(AriaRole.Heading, new() { Name = "Installation" })).ToBeVisibleAsync();
-        }
+        // Expects page to have a heading with the name of Installation.
+        await Expect(Page.GetByRole(AriaRole.Heading, new PageGetByRoleOptions {Name = "Installation"})).ToBeVisibleAsync();
     }
 }
